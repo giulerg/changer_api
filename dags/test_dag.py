3b371e6db51b181ae508a8309b1c36d8289a74b1
giulerg <henrytheanxious@gmail.com>
2026-06-14 15:31:47 +0300

    dag

---
+++ dags/test_dag.py
@@ -0,0 +1,17 @@
+from airflow  import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Hello, Airflow!")
+
+
+with DAG(
+    dag_id = "test_dag",
+    start_date=datetime(2024,1,1),
+    schedule="@daily"
+) as dag:
+    task = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello
+    )
